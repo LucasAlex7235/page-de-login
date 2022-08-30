f6623bfe96de3438570cc299205b52f11bdc9ad3
Lucas <lucasalex7235@gmail.com>
2022-08-30 13:37:13 -0300

    v0.4

--- src/script/index.js
+++ src/script/index.js
@@ -1,15 +1,49 @@
 class Login {
-    static checkPassword(){
+    static checkPassword() {
         const passwordModal = document.querySelectorAll("[data-control-modal]")
-        
-        passwordModal.forEach(button =>{
-            let modalId = button.getAttribute("data-control-modal")
 
-            button.addEventListener("click", ()=>{
+        passwordModal.forEach(button => {
+            let modalId = button.getAttribute("data-control-modal")
+            
+
+            button.addEventListener("click", () => {
                 document.getElementById(modalId).classList.toggle("show__modal")
             })
         })
     }
-}
+    static modal() {
+       
+        const container = document.querySelector(".container")
 
-Login.checkPassword()
+        const modalDiv = document.createElement("div")
+        const form = document.createElement("form")
+        const contentDiv = document.createElement("div")
+        const h3 = document.createElement("h3")
+        const p = document.createElement("p")
+        const input = document.createElement("input")
+        const button = document.createElement("button")
+
+
+
+        modalDiv.classList.add("modal-wrapper")
+        modalDiv.setAttribute("id", "modal-password")
+        contentDiv.classList.add("modal--content")
+        form.classList.add("modal")
+        input.type = "email"
+        input.classList.add("form--input")
+        input.placeholder = "Digite seu email"
+        button.classList.add("submit")
+
+        h3.innerText = "Esqueceu a senha?"
+        p.innerText = "Redefina a senha em duas etapas rápidas"
+        button.innerText = "Redefinir senha"
+
+        contentDiv.append(h3, p, input, button)
+        form.append(contentDiv)
+        modalDiv.append(form)
+        container.append(modalDiv)
+        
+        this.checkPassword()
+    }
+}
+Login.modal()
